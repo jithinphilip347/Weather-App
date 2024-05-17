2d11489dc45ce86ef4f45a8577bbbb5af92e721c
jithin philip <jithinphilip347@gmail.com>
2024-05-17 14:08:43 +0530

    responsive

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ const App = () => {
   return (
     <div className="flex justify-center items-center bg-gray-300 h-screen relative">
       {/* Absolute positioning for WeatherForm */}
-      <div className="w-[1200px] h-[500px] rounded-lg absolute bg-black opacity-100">
-        <img src={SkyCloud} alt="" className="w-[1200px] h-[500px] rounded-lg z-0" />
+      <div className="w-full h-full absolute inset-0 bg-black opacity-100">
+        <img src={SkyCloud} alt="" className="w-full h-full object-cover rounded-lg z-0" />
       </div>
       {/* Relative positioning for WeatherForm */}
       <div className="absolute inset-0 flex justify-center items-center">
@@ -16,4 +16,4 @@ const App = () => {
   )
 }
 
-export default App
+export default App;
